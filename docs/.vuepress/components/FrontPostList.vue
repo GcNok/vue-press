22fe66fc1649f0f3938c02abd896c5c0ba56b22d
Naoki <nshiji@smartshopping.co.jp>
2019-08-24 07:11:04 +0900

    ホバー時の色をオレンジに変更

--- docs/.vuepress/components/FrontPostList.vue
+++ docs/.vuepress/components/FrontPostList.vue
@@ -1,22 +1,45 @@
 <template>
-    <div>
-        <div v-for="post in posts">
-            <h2><a v-bind:href="post.path">{{post.title}}</a></h2>
-            <p>{{post.frontmatter.description}}</p>
-            <a v-bind:href="post.path">続きを読む</a>
-        </div>
+  <div>
+    <div v-for="post in posts">
+      <h2>
+        <a class="post-title" v-bind:href="post.path">{{post.title}}</a>
+      </h2>
+      <p>{{post.frontmatter.description}}</p>
+      <a class="read-more" v-bind:href="post.path">続きを読む</a>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    computed: {
-        posts() {
-            return this.$site.pages
-                // frontendディレクトリ配下のindex.md以外を表示
-                .filter(post => post.path.match(/\/frontend\/.+/))
-                // dateに設定した日付の降順にソートする
-                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
-        }
+  computed: {
+    posts() {
+      return (
+        this.$site.pages
+          // frontendディレクトリ配下のindex.md以外を表示
+          .filter(post => post.path.match(/\/frontend\/.+/))
+          // dateに設定した日付の降順にソートする
+          .sort(
+            (a, b) =>
+              new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+          )
+      );
     }
-}
+  }
+};
 </script>
+
+<style lang="stylus" scoped>
+.post-title {
+  font-weight: bold;
+
+  &:hover {
+    color: orange;
+  }
+}
+
+.read-more {
+  &:hover {
+    color: orange;
+  }
+}
+</style>
